1232c9da255eabe5143dbdb9b043626c9e6ed979
eduardpohrib01 <eduard.pohrib@s.unibuc.ro>
2020-12-02 15:49:35 +0200

    initial commit

---
+++ src/test/java/com/lifebeat/springbootvue/LifeBeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifebeat.springbootvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeBeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
